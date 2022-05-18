68a57bcc6190f12de20bd1f998a098c51dbaec14
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-18 09:53:05 +0000

    [Add]配送先追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,18 +1,38 @@
 class Public::AddressesController < ApplicationController
 
   def index
+    @addresses = Address.all
+    @address = Address.new
   end
 
   def create
+    address = Address.new(address_params)
+    address.customer_id = current_customer.id
+    address.save
+    redirect_to request.referer
   end
 
   def destroy
+    address = Address.find(params[:id])
+    address.customer_id = current_customer.id
+    address.destroy
+    redirect_to addresses_path
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def update
+    address = Address.find(params[:id])
+    address.customer_id = current_customer.id
+    address.update(address_params)
+    redirect_to addresses_path
   end
-
+  
+  private
+  def address_params
+    params.require(:address).permit(:name, :post_code, :address)
+  end 
 end
+
