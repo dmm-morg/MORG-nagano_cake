52cfaa75ac713ab6d697ee59650a8526587736f5
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-19 08:41:49 +0000

    [注文機能追加中]

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,7 +1,9 @@
 class Public::OrdersController < ApplicationController
 
   def new
+    @customer = current_customer
     @order = Order.new
+    
   end
 
   def confirm
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,13 @@
 class Address < ApplicationRecord
 
   belongs_to :customer
-  
+
   validates :name, presence: true
-  validates :post_code, presence: true, length: { is: 7 } 
+  validates :post_code, presence: true, length: { is: 7 }
   validates :address, presence: true
-  
+
   def address_display
     '〒' + post_code + '' + address + '' + name
   end
-  
+
 end
