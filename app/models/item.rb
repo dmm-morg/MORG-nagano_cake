bdbe02c0ba3b96f7722c0e16416110d1ed22f356
izumi-hiro <transformers2728frost@gmail.com>
2022-05-19 07:51:52 +0000

    [Add] validate

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,15 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details, dependent: :destroy
   belongs_to :genre
+  
+  with_options presence: true do
+    validate :name
+    validate :introduction
+    validate :tax_excluded_price
+    validate :sales_status
+    validate :genre_id
+    validate :image
+  end
 
   def tax_included_price
     (tax_excluded_price * 1.1).floor
