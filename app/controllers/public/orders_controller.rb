56b5c3bd9dc380a1405396808124a730e35fbd67
Mosa6 <tikamongi@gmail.com>
2022-05-21 09:59:45 +0000

    [update]orders_controller

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -11,7 +11,7 @@ class Public::OrdersController < ApplicationController
     if params[:order][:address_number] == "1"
       @order.post_code = current_customer.post_code
       @order.address = current_customer.address
-      @order.name = current_customer.first_name + current_customer.last_name
+      @order.name = current_customer.last_name + current_customer.first_name
     elsif params[:order][:address_number] == "2"
       @address = Address.find(params[:order][:address_id])
       @order.post_code = @address.post_code
