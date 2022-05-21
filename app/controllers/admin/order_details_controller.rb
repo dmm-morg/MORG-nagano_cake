7874ca9cad052311c32f71e1414be4a874f9e7be
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-21 12:04:01 +0000

    [Add]機能追加中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,8 +2,10 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    redirect_to request.referer
   end
-  
+
   def order_detail_params
     params.require(:order_detail).permit(:order_id, :item_id, :tax_included_price, :count, :making_status)
   end
