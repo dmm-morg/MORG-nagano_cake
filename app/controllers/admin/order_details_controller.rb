e6ffbeed4542667e256be5cce1bddf73cd9f9a47
yysyo111 <101189476+yysyo111@users.noreply.github.com>
2022-05-21 09:35:30 +0000

    [Add]機能追加中

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,6 +1,10 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
+    @order_detail = OrderDetail.find(params[:id])
   end
-
+  
+  def order_detail_params
+    params.require(:order_detail).permit(:order_id, :item_id, :tax_included_price, :count, :making_status)
+  end
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,18 @@
 class Admin::OrdersController < ApplicationController
 
   def show
+    @order = Order.find(params[:id])
+    @order.shipping_cost = 800
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to request.referer
   end
 
+  private
+  def order_params
+    params.require(:order).permit(:order_status, :payment_method, :post_code, :address, :name, :shipping_cost, :customer_id, :total_payment)
+  end
 end
