6497a4996cca07dc33b5ab013b4be5365d5883b4
Mosa6 <tikamongi@gmail.com>
2022-05-24 10:35:57 +0000

    [update]Error message

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,14 +4,9 @@ class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   has_many :items, through: :order_details
 
-<<<<<<< HEAD
-  validates :post_code, length: { is: 7 }
-  validates :name, :address, :post_code, presence: true
-=======
   validates :name, presence: true
   validates :post_code, presence: true, length: { is: 7 }
   validates :address, presence: true
->>>>>>> origin/develop
 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum order_status: { wait_payment: 0, confirm_payment: 1, making: 2, preparing: 3, shipped: 4 }
