328a24f002252ff1646d293443327a80acc5ecb2
Mosa6 <tikamongi@gmail.com>
2022-05-25 07:25:37 +0000

    [update]title

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -12,7 +12,7 @@ class Public::CustomersController < ApplicationController
   def update
     @customer = current_customer
     if @customer.update(customer_params)
-      redirect_to customers_my_page_path
+      redirect_to customers_my_page_path, notice: "更新に成功しました"
     else
       render "edit"
     end
